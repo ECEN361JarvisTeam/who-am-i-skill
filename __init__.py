91017c9fef599a367bf70d4d48f001c70e843c7d
ECEN361JarvisTeam <noahkfish@gmail.com>
2021-11-12 13:42:09 -0700

    Test mycroft prompt functionality.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+from behave import given
+from test.integrationtests.voight_kampff import wait_for_dialog, emit_utterance
 
 
 class WhoAmI(MycroftSkill):
@@ -7,7 +9,16 @@ class WhoAmI(MycroftSkill):
 
     @intent_file_handler('i.am.who.intent')
     def handle_i_am_who(self, message):
-        self.speak_dialog('i.am.who')
+        recognized = False;
+        # Check for matching image
+        
+        if recognized:
+            self.speak_dialog('i.am.who')
+        else:
+            self.speak_dialog('i.am.who.unknown')
+            # Code to add new face.
+	    name = self.get_response('i.am')
+            self.speak_dialog('i.am.who.known', {'name': name})
 
 
 def create_skill():
