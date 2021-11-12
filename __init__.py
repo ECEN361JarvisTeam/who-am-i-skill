d05e4c0eb301c4994e28203e2d6f9c83ab5ef587
ECEN361JarvisTeam <noahkfish@gmail.com>
2021-11-12 14:37:39 -0700

    Whitespace

--- __init__.py
+++ __init__.py
@@ -11,14 +11,14 @@ class WhoAmI(MycroftSkill):
     def handle_i_am_who(self, message):
         recognized = False;
         # Check for matching image
-	
+
         if recognized:
-		self.speak_dialog('i.am.who')
-	else:
-		self.speak_dialog('i.am.who.unknown')
-		# Code to add new face.
-		name = self.get_response('i.am.intent')
-		self.speak_dialog('i.am.who.known', {'name': name})
+            self.speak_dialog('i.am.who')
+        else:
+            self.speak_dialog('i.am.who.unknown')
+            # Code to add new face.
+            name = self.get_response('i.am.intent')
+            self.speak_dialog('i.am.who.known', {'name': name})
 
 
 def create_skill():
